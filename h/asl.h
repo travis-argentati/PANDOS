509ecb5284bf80983b89e2f1d3938bf46999bb01
travis-argentati <argentatit@xavier.edu>
2020-09-06 21:48:40 -0400

    asl psuedo code

--- h/asl.h
+++ h/asl.h
@@ -11,10 +11,10 @@
 
 #include "../h/types.h"
 
-extern int insertBlocked (int *semAdd, pcb_PTR p);
-extern pcb_PTR removeBlocked (int *semAdd);
-extern pcb_PTR outBlocked (pcb_PTR p);
-extern pcb_PTR headBlocked (int *semAdd);
+extern int insertBlocked (int *semAdd, pcb_t *p);
+extern pcb_t *removeBlocked (int *semAdd);
+extern pcb_t *outBlocked (pcb_t *p);
+extern pcb_t *headBlocked (int *semAdd);
 extern void initASL ();
 
 /***************************************************************/
